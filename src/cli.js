b5cfc4b7a5ed4c7c8457ab4b40b7fc1682c1671a
Artur Drobinskiy <artur.drobinskiy@gmail.com>
2021-03-22 22:33:38 +0700

    convert line endings to lf

--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,7 @@ const { exec } = require('child_process');
 const args = process.argv.splice(2).join(' ');
 const pathToTemplates = process.argv[1].replace('cli.js', 'templates');
 const isYarn = process.env.npm_execpath.includes('yarn');
-const toExecute = `${isYarn ? 'yarn' : 'npm run'} nswag openapi2tsclient /templateDirectory:${pathToTemplates} ${args}`
+const toExecute = `${
+  isYarn ? 'yarn' : 'npm run'
+} nswag openapi2tsclient /templateDirectory:${pathToTemplates} ${args}`;
 exec(toExecute);
