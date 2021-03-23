aa32ecba92fc7f485fc9d04467ed95d5943249f7
Sergey Metlenkin <sergei.metlenkin@mcc-tomsk.de>
2021-03-23 10:01:43 +0700

    Works in yarn workspaces environment

--- src/cli.js
+++ src/cli.js
@@ -3,8 +3,11 @@
 const { execSync } = require('child_process');
 
 const args = process.argv.splice(2).join(' ');
-const pathToTemplates = process.argv[1].replace('cli.js', 'templates');
+const pathToTemplates = process.argv[1]
+  .replace('cli.js', 'templates')
+  .replace('.bin/react-query-swagger', 'react-query-swagger/templates');
 const isYarn = process.env.npm_execpath.includes('yarn');
+
 const toExecute = `${
   isYarn ? 'yarn' : 'npm run'
 } nswag openapi2tsclient /templateDirectory:${pathToTemplates} ${args}`;
