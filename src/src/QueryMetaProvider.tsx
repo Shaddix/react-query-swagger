c38e6e287e212e0b8a1040463fd71e167408e76f
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2022-04-25 17:22:50 +0700

    enrich parent Meta

--- src/src/QueryMetaProvider.tsx
+++ src/src/QueryMetaProvider.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useMemo } from 'react';
+import React, { PropsWithChildren, useContext, useMemo } from 'react';
 import { QueryMeta } from 'react-query';
 
 type QueryScopeProps = PropsWithChildren<{
@@ -10,6 +10,12 @@ type QueryScopeProps = PropsWithChildren<{
    * metaFn has priority over meta prop (i.e. if both are defined, metaFn is used)
    */
   metaFn?: () => QueryMeta;
+
+  /*
+   * If true Metas from parent's QueryMetaContexts will be erased.
+   * By default new Metas are appended to the parent's one.
+   */
+  resetParentMeta?: boolean;
 }>;
 
 export const QueryMetaContext = React.createContext<{
@@ -21,10 +27,21 @@ export const QueryMetaContext = React.createContext<{
  * All queries within children of this scope will have Meta set to either `meta` prop or the result of `metaFn()`.
  */
 export const QueryMetaProvider: React.FC<QueryScopeProps> = (props) => {
+  const parentMeta = useContext(QueryMetaContext);
   const value = useMemo(() => {
-    const metaFn = props.metaFn ?? (props.meta ? () => props.meta! : undefined);
-    return { metaFn };
-  }, [props.metaFn, props.meta]);
+    const metaFn = () => {
+      const meta = props.metaFn ? props.metaFn() : props.meta;
+      return {
+        ...(props.resetParentMeta || !parentMeta.metaFn
+          ? {}
+          : parentMeta.metaFn()),
+        ...meta,
+      };
+    };
+    return {
+      metaFn,
+    };
+  }, [parentMeta.metaFn, props.metaFn, props.meta, props.resetParentMeta]);
 
   return (
     <QueryMetaContext.Provider value={value}>
